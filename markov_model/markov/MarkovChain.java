8da718ac89e544f03a956b1d13d223ec0c66968a
Kolya Souvorin <souvorinkg@hendrix.edu>
2024-04-01 14:39:50 -0500

    Markov Model, youtube comments
    Markvov Model finished, youtube comments being pulled from API

---
+++ markov_model/markov/MarkovChain.java
@@ -0,0 +1,107 @@
+package markov_model.markov;
+
+import markov_model.core.Histogram;
+
+import java.util.*;
+
+public class MarkovChain<L,S> {
+    private LinkedHashMap<L, HashMap<Optional<S>, Histogram<S>>> label2symbol2symbol = new LinkedHashMap<>();
+
+    public Set<L> allLabels() {return label2symbol2symbol.keySet();}
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (L language: label2symbol2symbol.keySet()) {
+            sb.append(language);
+            sb.append('\n');
+            for (Map.Entry<Optional<S>, Histogram<S>> entry: label2symbol2symbol.get(language).entrySet()) {
+                sb.append("    ");
+                sb.append(entry.getKey());
+                sb.append(":");
+                sb.append(entry.getValue().toString());
+                sb.append('\n');
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
+    }
+
+    // Increase the count for the transition from prev to next.
+    // Should pass SimpleMarkovTest.testCreateChains().
+    public void count(Optional<S> prev, L label, S next) {
+        HashMap<Optional<S>, Histogram<S>> symbol2symbol = label2symbol2symbol.computeIfAbsent(label, k -> new HashMap<>());
+
+        Histogram<S> histogram = symbol2symbol.get(prev);
+        if (histogram == null) {
+            histogram = new Histogram<>();
+            symbol2symbol.put(prev, histogram);
+        }
+
+        histogram.bump(next);
+    }
+
+
+    // HINT: Be sure to add 1 to both the numerator and denominator when finding the probability of a
+    // transition. This helps avoid sending the probability to zero.
+    public double probability(ArrayList<S> sequence, L label) {
+        double logProbabilitySum;
+        logProbabilitySum = 0.0;
+
+        HashMap<Optional<S>, Histogram<S>> symbol2symbol = label2symbol2symbol.get(label);
+        if (symbol2symbol != null) {
+            Optional<S> prev = Optional.empty();
+
+            for (S currentSymbol : sequence) {
+                Histogram<S> histogram = symbol2symbol.get(prev);
+
+                if (histogram != null) {
+                    double prob = (histogram.getCountFor(currentSymbol) + 1.0) / (histogram.getTotalCounts() + 1.0
+                    );
+                    double logProb = Math.log(prob);
+                    logProbabilitySum += logProb;
+                }
+
+                prev = Optional.of(currentSymbol);
+            }
+        }
+
+        return Math.exp(logProbabilitySum); //reconvert
+    }
+
+    // Return a map from each label to P(label | sequence).
+    // Should pass MajorMarkovTest.testSentenceDistributions()
+    public LinkedHashMap<L, Double> labelDistribution(ArrayList<S> sequence) {
+        LinkedHashMap<L, Double> distribution = new LinkedHashMap<>();
+        double totalProbability = 0.0;
+
+        for (L label : allLabels()) {
+            double labelProb = probability(sequence, label);
+            distribution.put(label, labelProb);
+            totalProbability += labelProb;
+        }
+
+        for (Map.Entry<L, Double> entry : distribution.entrySet()) {
+            distribution.put(entry.getKey(), entry.getValue() / totalProbability);
+        }
+
+        return distribution;
+    }
+
+    // Calls labelDistribution(). Returns the label with the highest probability.
+    // Should pass MajorMarkovTest.bestChainTest()
+    public L bestMatchingChain(ArrayList<S> sequence) {
+        LinkedHashMap<L, Double> allLang = labelDistribution(sequence);
+        L bestChoice = null;
+        double maxProbability = 0.0;
+
+        for (Map.Entry<L, Double> entry : allLang.entrySet()) {
+            if (entry.getValue() > maxProbability) {
+                maxProbability = entry.getValue();
+                bestChoice = entry.getKey();
+            }
+        }
+
+        return bestChoice;
+    }
+}
---
+++ markov_model/markov/MarkovLanguage.java
@@ -0,0 +1,53 @@
+package markov_model.markov;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class MarkovLanguage extends MarkovChain<String,Character> {
+
+    public void countFrom(File languageFile, String language) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(languageFile));
+        Optional<Character> prev = Optional.empty();
+        for (;;) {
+            int read = reader.read();
+            if (read < 0) {
+                break;
+            } else {
+                Optional<Character> next = usableCharacter((char)read);
+                if (next.isPresent()) {
+                    char c = next.get();
+                    count(prev, language, c);
+                    prev = next;
+                }
+            }
+        }
+    }
+
+    public static <T> Optional<T> usableWord(Optional<T> c) {
+        return Optional.empty();
+    }
+
+    public static Optional<Character> usableCharacter(char c) {
+        c = Character.toLowerCase(c);
+
+        if (c >= 'a' && c <= 'z') { // Restrict to English Latin alphabet
+            return Optional.of(c);
+        } else if (Character.isSpaceChar(c)) {
+            return Optional.of(' ');
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static ArrayList<Character> usableCharacters(String input) {
+        ArrayList<Character> result = new ArrayList<>();
+        for (char c: input.toCharArray()) {
+            usableCharacter(c).ifPresent(result::add);
+        }
+        return result;
+    }
+}
